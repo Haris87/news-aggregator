4b5c4accab7b6976571de1857450ecf8aaff154b
Haris87 <mpouxlhs_xar@yahoo.com>
2018-03-10 13:12:52 +0200

    newsitem converts array

--- main.js
+++ main.js
@@ -34,55 +34,52 @@ var cointelegraph = new Source({
 
 function allFeeds(source) {
 	source.allFeeds().then(function(result) {
-		var merged = [].concat.apply([], result);
-		var topics = Topic.getTopics(merged)[0];
-		console.log("\x1b[35m%s\x1b[0m", "ALL FEEDS", result);
-		console.log("\x1b[35m%s\x1b[0m", "TOPICS", topics);
+
+		var twitter = result[0];
+		var rss = result[1];
+		var medium = result[2];
+		var reddit = result[3];
+
+
+		// var merged = [].concat.apply([], result);
+		// var topics = Topic.getTopics(merged)[0];
+		console.log("\x1b[35m%s\x1b[0m", "TWITTER", twitter[0]);
+		// console.log("\x1b[35m%s\x1b[0m", "RSS", rss[0]);
+		console.log("\x1b[35m%s\x1b[0m", "MEDIUM", medium[0]);
+		console.log("\x1b[35m%s\x1b[0m", "reddit", reddit[0]);
 	});
 }
 
 function twitterFeed(source){
 	source.twitterFeed().then(function(result) {
-		// var merged = [].concat.apply([], result);
-		// var topics = Topic.getTopics(merged)[0];
 		console.log(result[1]);
 		console.log("\x1b[35m%s\x1b[0m", "TWEET[1]", NewsItem.fromTwitter(result[1], source.name));
-		// console.log("\x1b[35m%s\x1b[0m", "TOPICS", topics);
 	});
 }
 
 function redditFeed(source){
 	source.redditFeed().then(function(result) {
-		// var merged = [].concat.apply([], result);
-		// var topics = Topic.getTopics(merged)[0];
 		console.log(result[0]);
 		console.log("\x1b[35m%s\x1b[0m", "REDDIT[0]", NewsItem.fromReddit(result[0], source.name));
-		// console.log("\x1b[35m%s\x1b[0m", "TOPICS", topics);
 	});
 }
 
 function mediumFeed(source){
 	source.mediumFeed().then(function(result) {
-		// var merged = [].concat.apply([], result);
-		// var topics = Topic.getTopics(merged)[0];
 		console.log(result[0]);
 		console.log("\x1b[35m%s\x1b[0m", "MEDIUM[0]", NewsItem.fromMedium(result[0], source.name));
-		// console.log("\x1b[35m%s\x1b[0m", "TOPICS", topics);
 	});
 }
 
 function rssFeed(source){
 	source.rssFeed().then(function(result) {
-		// var merged = [].concat.apply([], result);
-		// var topics = Topic.getTopics(merged)[0];
 		console.log(result[0]);
 		console.log("\x1b[35m%s\x1b[0m", "RSS[0]", NewsItem.fromRSS(result[0], source.name));
-		// console.log("\x1b[35m%s\x1b[0m", "TOPICS", topics);
 	});
 }
 
-rssFeed(cointelegraph);
+// rssFeed(cointelegraph);
 // mediumFeed(webetcrypto);
 // redditFeed(webetcrypto);
 // twitterFeed(molyneux);
-// allFeeds(webetcrypto);
+allFeeds(webetcrypto);
--- news-item.js
+++ news-item.js
@@ -4,72 +4,102 @@ var cheerio = require('cheerio');
 
 class NewsItem {
 
-	constructor(obj) {
-		this.title = obj.title || null;
-		this.content = obj.content || null;
-		this.media = obj.media || null;
+  constructor(obj) {
+    this.title = obj.title || null;
+    this.content = obj.content || null;
+    this.media = obj.media || null;
     this.author = obj.author || null;
     this.url = obj.url || null;
-		this.dateCreated = obj.dateCreated || null;
-		this.dateAdded = obj.dateAdded || null;
+    this.dateCreated = obj.dateCreated || null;
+    this.dateAdded = obj.dateAdded || null;
     this.type = obj.type || null;
     this.source = obj.source || null;
-	}
-
-  static fromTwitter(tweet, source){
-    var obj = {};
-    obj.title = tweet.text;
-		obj.content = tweet.text;
-		obj.media = tweet.entities.media ? tweet.entities.media[0].media_url : null;
-    obj.author = tweet.user.screen_name;
-    obj.url = "https://twitter.com/"+tweet.user.screen_name+"/status/"+tweet.id_str;
-		obj.dateCreated = parseTwitterDate(tweet.created_at);
-		obj.dateAdded = new Date();
-    obj.type = 'TWEET';
-    obj.source = source;
-    return new NewsItem(obj);
   }
 
-  static fromReddit(feed, source){
-    var obj = {};
-    obj.title = feed.title;
-		obj.content = feed.content;
-		obj.media = null;
-    obj.author = feed.author;
-    obj.url = feed.link;
-		obj.dateCreated = feed.pubDate;
-		obj.dateAdded = new Date();
-    obj.type = 'REDDIT';
-    obj.source = source;
-    return new NewsItem(obj);
+  static fromTwitter(tweets, source) {
+    var result = [];
+    tweets.forEach(function(tweet) {
+      var obj = {};
+      obj.title = tweet.text;
+      obj.content = tweet.text;
+      obj.media = tweet.entities.media ? tweet.entities.media[0].media_url : null;
+      obj.author = tweet.user.screen_name;
+      obj.url = "https://twitter.com/" + tweet.user.screen_name + "/status/" + tweet.id_str;
+      obj.dateCreated = parseTwitterDate(tweet.created_at);
+      obj.dateAdded = new Date();
+      obj.type = 'TWEET';
+      obj.source = source;
+      result.push(new NewsItem(obj));
+    });
+    return result;
+
   }
 
-  static fromMedium(feed, source){
-    var obj = {};
-    obj.title = feed.title;
-		obj.content = feed['content:encoded'];
-		obj.media = null;
-    obj.author = feed.creator;
-    obj.url = feed.link;
-		obj.dateCreated = new Date(feed.pubDate);
-		obj.dateAdded = new Date();
-    obj.type = 'MEDIUM';
-    obj.source = source;
-    return new NewsItem(obj);
+  static fromReddit(feed, source) {
+    var result = [];
+    feed.forEach(function(item) {
+
+      var obj = {};
+      obj.title = item.title;
+      obj.content = item.content;
+      obj.media = null;
+      obj.author = item.author;
+      obj.url = item.link;
+      obj.dateCreated = item.pubDate;
+      obj.dateAdded = new Date();
+      obj.type = 'REDDIT';
+      obj.source = source;
+      result.push(new NewsItem(obj));
+    });
+    return result;
+
   }
 
-  static fromRSS(feed, source){
-    var obj = {};
-    obj.title = feed.title;
-		obj.content = feed.content || feed.contentSnippet;
-		obj.media = feed.image ? feed.image.url : null ;
-    obj.author = feed.creator || feed.author || feed['dc:creator'];
-    obj.url = feed.link || feed.url;
-		obj.dateCreated = feed.pubDate ? new Date(feed.pubDate) : null;
-		obj.dateAdded = new Date();
-    obj.type = 'RSS';
-    obj.source = source;
-    return new NewsItem(obj);
+  static fromMedium(feed, source) {
+    var result = [];
+    feed.forEach(function(item) {
+
+      var obj = {};
+      obj.title = item.title;
+      obj.content = item['content:encoded'];
+      obj.media = null;
+      obj.author = item.creator;
+      obj.url = item.link;
+      obj.dateCreated = new Date(item.pubDate);
+      obj.dateAdded = new Date();
+      obj.type = 'MEDIUM';
+      obj.source = source;
+      result.push(new NewsItem(obj));
+    });
+    return result;
+
+  }
+
+  static fromRSS(feed, source) {
+    var result = [];
+    feed.forEach(function(item) {
+
+      var obj = {};
+      obj.title = item.title;
+      obj.content = item.content || item.contentSnippet;
+      obj.media = item.image ? item.image.url : null;
+      obj.author = item.creator || item.author || item['dc:creator'];
+      obj.url = item.link || item.url;
+      obj.dateCreated = item.pubDate ? new Date(item.pubDate) : null;
+      obj.dateAdded = new Date();
+      obj.type = 'RSS';
+      obj.source = source;
+      result.push(new NewsItem(obj));
+    });
+    return result;
+
+  }
+
+  static allSources(sources) {
+    var twitter = sources[0];
+    var rss = sources[1];
+    var medium = sources[2];
+    var reddit = sources[3];
   }
 
 }
@@ -78,7 +108,7 @@ function stripHtml(str) {
   return str.replace(/<(?:.|\n)*?>/gm, '');
 }
 
-function getRedditImage(content){
+function getRedditImage(content) {
   var $ = cheerio.load(content);
   return $('img');
 }
